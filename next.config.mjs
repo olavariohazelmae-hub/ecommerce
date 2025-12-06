74076fab72d241b66d19ff544b15c58fc9dd1123
olavariohazelmae-hub <olavariohazelmae@gmail.com>
2025-12-05 23:34:46 -0500

    test

--- next.config.mjs
+++ next.config.mjs
@@ -64,7 +64,7 @@ const nextConfig = {
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "img-src 'self' data: blob: https: http:",
               "font-src 'self' data: https://fonts.gstatic.com",
-              "connect-src 'self' https://*.supabase.co https://www.google-analytics.com https://api.stripe.com",
+              "connect-src 'self' https://*.supabase.co https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com https://api.stripe.com",
               "frame-src 'self' https://js.stripe.com",
               "object-src 'none'",
               "base-uri 'self'",
